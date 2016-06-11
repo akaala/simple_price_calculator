30c31eba1a864c9680be1c2eeb5f6099f936b0e1
akaala <jacobke2012@gmail.com>
2016-06-11 11:01:07 +0800

    简单重构代码，增加递归中单次结果集的大小限制。

--- src/main/java/BetterCalculator.java
+++ src/main/java/BetterCalculator.java
@@ -11,12 +11,42 @@ import java.util.Scanner;
 public class BetterCalculator {
 
     public static void main(String[] args) {
-        String totalString = readInput("本程序可用于计算'不定方程的非负整数解问题'。\n首先，输入总价：");
+        double total = inputTotal();
+        List<Double> prices = inputPrices();
+
+        System.out.println("总价为：" + total);
+        System.out.println("各物品单价为：" + prices);
+        System.out.println("计算开始……");
+
+        List<List<Integer>> results = morePrices(prices, total, 2);
+        System.out.println("计算结束，可能的组合为：");
+
+        outputResult(total, prices, results);
+
+        System.out.println("程序结束。");
+        readInput("按任意键结束.\nCopyright By: Jacob Ke. Date: 2016-6-10");
+    }
+
+    private static void outputResult(double total, List<Double> prices, List<List<Integer>> results) {
+        for (List<Integer> result : results) {
+            assert (result.size() == prices.size());
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.size(); i++) {
+                sb.append(prices.get(i)).append(" * ").append(result.get(i)).append(" + ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append(" = ").append(total);
+            System.out.println(sb);
+        }
+    }
+
+    private static List<Double> inputPrices() {
+        List<Double> prices = Lists.newArrayList();
         String pricesString = readInput("输入单价（用','分割）：");
 
-        List<Double> prices = Lists.newArrayList();
 
-        double total = Double.parseDouble(totalString);
         try {
             for (String s : Splitter.on(",").trimResults().splitToList(pricesString)) {
                 prices.add(Double.parseDouble(s));
@@ -25,36 +55,20 @@ public class BetterCalculator {
             System.out.println("输入不是数字。退出！");
             System.exit(3);
         }
-
-        System.out.println("总价为：" + total);
-        System.out.println("各物品单价为：" + prices);
-        System.out.println("计算开始……");
-
-        List<List<Integer>> results = morePrice(prices, total);
-        System.out.println("计算结束，可能的组合为：");
-
-        for (List<Integer> result : results) {
-            assert (result.size() == prices.size());
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < result.size(); i++) {
-                sb.append(prices.get(i)).append(" * ").append(result.get(i)).append(" + ");
-            }
-            sb.deleteCharAt(sb.length() - 1);sb.deleteCharAt(sb.length() - 1);
-            sb.append(" = ").append(total);
-            System.out.println(sb);
-        }
-
-        System.out.println("程序结束。");
-        readInput("按任意键结束.\nCopyright By: Jacob Ke. Date: 2016-6-10");
+        return prices;
     }
 
-    public static List<List<Integer>> morePrice(List<Double> prices, double total) {
+    private static double inputTotal() {
+        String totalString = readInput("本程序可用于计算'不定方程的非负整数解问题'。\n首先，输入总价：");
+        return Double.parseDouble(totalString);
+    }
+
+    public static List<List<Integer>> morePrices(List<Double> prices, double total, int maxResultSize) {
         List<List<Integer>> result = Lists.newArrayList();
         assert prices.size() >= 2;
 
         if (prices.size() == 2) {
-            result = towPrice(prices.get(0), prices.get(1), total);
+            result = towPrices(prices.get(0), prices.get(1), total, maxResultSize);
         } else {
             for (Double price : prices) {
                 for (int count = 1; count <= total / price; count++) {
@@ -64,7 +78,7 @@ public class BetterCalculator {
                     newPrices.remove(removedIndex);
 
                     double nextTotal = total - count * price;
-                    List<List<Integer>> counters = morePrice(newPrices, nextTotal);
+                    List<List<Integer>> counters = morePrices(newPrices, nextTotal, maxResultSize);
 
                     if (counters != null && counters.size() != 0) {
                         for (List<Integer> counter : counters) {
@@ -80,12 +94,15 @@ public class BetterCalculator {
         return result;
     }
 
-    public static List<List<Integer>> towPrice(double price1, double price2, double total) {
+    public static List<List<Integer>> towPrices(double price1, double price2, double total, int maxResultSize) {
         List<List<Integer>> result = Lists.newArrayList();
         for (int c1 = 0; c1 <= total / price1; c1++) {
             for (int c2 = 0; c2 <= total / price2; c2++) {
                 if (price1 * c1 + price2 * c2 == total) {
                     result.add(Lists.newArrayList(c1, c2));
+                }
+                if (result.size() >= maxResultSize) {
+                    return result;
                 }
             }
         }
